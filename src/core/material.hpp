ee9d9170bfa27e7e3389cd824836bab7327f035a
Tiago Gomes <tiago.gomes_mtg@hotmail.com>
2015-01-31 13:21:20 -0200

    Added material with support for diffuse and specular components.

---
+++ src/core/material.hpp
@@ -0,0 +1,31 @@
+#ifndef _CORE_MATERIAL_HPP
+#define _CORE_MATERIAL_HPP
+
+#include <graphics/color.hpp>
+
+#include <tuple>
+
+namespace core
+{
+
+struct material
+{
+  graphics::color diffuse_color;
+  graphics::color specular_color;
+  float specular_exponent;
+};
+
+inline bool operator==(material lhs, material rhs)
+{
+  return std::tie(lhs.diffuse_color, lhs.specular_color, lhs.specular_exponent)
+    == std::tie(rhs.diffuse_color, rhs.specular_color, rhs.specular_exponent);
+}
+
+inline bool operator!=(material lhs, material rhs)
+{
+  return !(lhs == rhs);
+}
+
+}
+
+#endif
