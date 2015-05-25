6a039a19f3453bcb6b0991cdc99afe90ee24646c
Tiago Gomes <tiago.gomes_mtg@hotmail.com>
2015-05-24 22:56:40 -0300

    Adds function to translate a point.

--- src/math/utility.hpp
+++ src/math/utility.hpp
@@ -6,10 +6,15 @@
 
 namespace math
 {
-
+  
 inline vector3d make_vector3d(point3d from, point3d to)
 {
   return { to.x - from.x, to.y - from.y, to.z - from.z };
+}
+
+inline point3d translated(point3d point, vector3d direction)
+{
+  return{ point.x + direction.x, point.y - direction.y, point.z - direction.z };
 }
 
 }
