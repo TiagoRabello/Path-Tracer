8fcbb4a0d4d01eb274f69da3963bd14b0765d72f
Tiago Gomes <tiago.gomes_mtg@hotmail.com>
2015-05-24 22:54:18 -0300

    Adds missing inline keywords.

--- src/math/trigonometry.hpp
+++ src/math/trigonometry.hpp
@@ -11,16 +11,16 @@ namespace math
 
 const auto pi = 3.14159265359f;
 
-radians to_radians(degrees angle) { return { angle.value * pi / 180.0f }; }
-degrees to_degrees(radians angle) { return { angle.value * 180.0f / pi }; }
+inline radians to_radians(degrees angle) { return { angle.value * pi / 180.0f }; }
+inline degrees to_degrees(radians angle) { return { angle.value * 180.0f / pi }; }
 
-float sin(radians angle) { return std::sin(angle.value); }
-float cos(radians angle) { return std::cos(angle.value); }
-float tan(radians angle) { return std::tan(angle.value); }
+inline float sin(radians angle) { return std::sin(angle.value); }
+inline float cos(radians angle) { return std::cos(angle.value); }
+inline float tan(radians angle) { return std::tan(angle.value); }
 
-float sin(degrees angle) { return sin(to_radians(angle)); }
-float cos(degrees angle) { return cos(to_radians(angle)); }
-float tan(degrees angle) { return tan(to_radians(angle)); }
+inline float sin(degrees angle) { return sin(to_radians(angle)); }
+inline float cos(degrees angle) { return cos(to_radians(angle)); }
+inline float tan(degrees angle) { return tan(to_radians(angle)); }
 
 }
 
