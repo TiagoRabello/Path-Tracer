43ca79b3e797488d63ea5ecb8a16940ab9e0cc9c
Tiago Gomes <tiago.gomes_mtg@hotmail.com>
2016-06-04 20:21:33 -0300

    Adds a class to represent a coordinate system in 3d space.

---
+++ src/math/coordinate_system.hpp
@@ -0,0 +1,34 @@
+#ifndef MATH_COORDINATE_SYSTEM
+#define MATH_COORDINATE_SYSTEM
+
+#include <math/vector3d.hpp>
+#include <cmath>
+
+namespace math
+{
+  struct coordinate_system
+  {
+    math::vector3d v1;
+    math::vector3d v2;
+    math::vector3d v3;
+  };
+
+  inline coordinate_system make_coordinate_system(math::vector3d v1)
+  {
+    math::vector3d v2;
+    if (std::abs(v1.x) > std::abs(v1.y))
+    {
+      float inv_length = 1.0f / std::sqrt(v1.x * v1.x + v1.z * v1.z);
+      v2 = { -v1.z * inv_length, 0.0f, v1.x * inv_length };
+    }
+    else
+    {
+      float inv_length = 1.0f / std::sqrt(v1.y * v1.y + v1.z * v1.z);
+      v2 = { 0.0f, v1.z * inv_length, -v1.y * inv_length };
+    }
+
+    return{ v1, v2, math::cross(v1, v2) };
+  }
+}
+
+#endif
